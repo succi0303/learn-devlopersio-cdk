5f56c3a6f9f969535c4f6812dfd788ddff540cee
tasugim <tasugim@amazon.co.jp>
2021-08-09 10:37:53 +0900

    #5 サブネット

--- test/devio.test.ts
+++ test/devio.test.ts
@@ -17,3 +17,45 @@ test('Vpc', () => {
     Tags: [{ 'Key': 'Name', 'Value': 'starwars-prd-vpc' }]
   }));
 });
+
+test('Subnet', () => {
+  const app = new cdk.App({
+    context: {
+      'systemName': 'starwars',
+      'envType': 'prd'
+    }
+  });
+  const stack = new Devio.DevioStack(app, 'DevioStack');
+
+  expectCDK(stack).to(countResources('AWS::EC2::Subnet', 6));
+  expectCDK(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.11.0/24',
+    AvailabilityZone: 'ap-northeast-1a',
+    Tags: [{ 'Key': 'Name', 'Value': 'starwars-prd-subnet-public-1a' }]
+  }));
+  expectCDK(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.12.0/24',
+    AvailabilityZone: 'ap-northeast-1c',
+    Tags: [{ 'Key': 'Name', 'Value': 'starwars-prd-subnet-public-1c' }]
+  }));
+  expectCDK(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.21.0/24',
+    AvailabilityZone: 'ap-northeast-1a',
+    Tags: [{ 'Key': 'Name', 'Value': 'starwars-prd-subnet-app-1a' }]
+  }));
+  expectCDK(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.22.0/24',
+    AvailabilityZone: 'ap-northeast-1c',
+    Tags: [{ 'Key': 'Name', 'Value': 'starwars-prd-subnet-app-1c' }]
+  }));
+  expectCDK(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.31.0/24',
+    AvailabilityZone: 'ap-northeast-1a',
+    Tags: [{ 'Key': 'Name', 'Value': 'starwars-prd-subnet-db-1a' }]
+  }));
+  expectCDK(stack).to(haveResource('AWS::EC2::Subnet', {
+    CidrBlock: '10.0.32.0/24',
+    AvailabilityZone: 'ap-northeast-1c',
+    Tags: [{ 'Key': 'Name', 'Value': 'starwars-prd-subnet-db-1c' }]
+  }));
+})
